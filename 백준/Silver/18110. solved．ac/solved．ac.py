79cd8fe6cc154baa5b959e4a61402bccb61265f7
YooSeungJun <40661651+tmdwns29@users.noreply.github.com>
2024-02-18 23:05:18 +0900

    [Silver IV] Title: solved.ac, Time: 156 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,15 +1,16 @@
 import sys
-input = sys.stdin.readline
- 
-def round2(num):
+N = int(input())
+
+def banolim(num):
     return int(num) + (1 if num - int(num) >= 0.5 else 0)
- 
-n = int(input())
-if not n:
-    print(0)
+
+if not N: print(0)
 else:
-    score = [int(input()) for _ in range(n)]
-    trunc = round2(n * 0.15)
-    apply_trunc = sorted(score)[trunc: n - trunc]
-    average = round2(sum(apply_trunc) / len(apply_trunc))
-    print(average)
+    num = [int(sys.stdin.readline()) for _ in range(N)]
+    A = banolim(N * 0.15)
+    num.sort()
+    print(banolim(sum(num[A:-A] if A else num)/(N-(A*2))))
+    ''' A가 0이 아니라면 num[A:-A]의 합
+    A가 0이라면 num전체 합
+    A가 0이면 나누는 수도 num전체 길이로 나눔(N)
+    부동소수점?'''
