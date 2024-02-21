2ba08326145a84ba3c811e52f8f1a328ea8aa049
YooSeungJun <40661651+tmdwns29@users.noreply.github.com>
2024-02-21 12:17:48 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,13 +1,7 @@
-N = int(input())
+L = int(input())
 arr = input()
-alphabet = ' abcdefghijklmnopqrstuvwxyz'
+result = 0
 
-def Hash(num, x):
-    result = 0
-    for i in range(1, num+1):
-        for j in alphabet:
-            if x[i-1] == j:
-                result += alphabet.index(j)*(31**(i-1))
-    return result
-
-print(Hash(N,arr))
+for i in range(L):
+    result += (ord(arr[i])-96)*(31**i) # ord()->아스키코드값 반환
+print(result % 1234567891) # M = 1234567891
