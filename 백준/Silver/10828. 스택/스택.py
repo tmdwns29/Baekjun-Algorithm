5b462908e98cb1f9ebe0ace4d804edd04d64a0c9
YooSeungJun <40661651+tmdwns29@users.noreply.github.com>
2024-02-23 19:48:28 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+value = 0
+stack = []
+for i in range(N):
+    command = sys.stdin.readline().rstrip()
+    
+    if 'push' in command:
+        command, value = command.split(' ')
+        stack.append(int(value))
+    elif 'pop' in command:
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+            stack.pop(-1)
+    elif 'size' in command:
+        print(len(stack))
+    elif 'empty' in command:
+        if len(stack) == 0: print(1)
+        else: print(0)
+    elif 'top' in command:
+        if len(stack) == 0: print(-1)
+        else: print(stack[-1])
