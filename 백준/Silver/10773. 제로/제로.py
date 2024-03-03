94bf68956ea0b64dafc6a03e5fed40a8859ee72a
YooSeungJun <40661651+tmdwns29@users.noreply.github.com>
2024-03-03 13:51:34 +0900

    [Silver IV] Title: 제로, Time: 136 ms, Memory: 114236 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,12 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 arr=[]
+
 for i in range(N):
     x = int(input())
     if x == 0:
-        arr.pop(-1)
+        arr.pop()
     else:
         arr.append(x)
 print(sum(arr))
