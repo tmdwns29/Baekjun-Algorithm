4d944cacbca07ee38a1fe82bea0851dedd53b3ff
YooSeungJun <12000ha@naver.com>
2024-03-12 14:54:10 +0900

    Update ATM.py

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -4,14 +4,14 @@ input = sys.stdin.readline
 N = int(input())
 P = list(map(int, input().split()))
 
-num,j = [], 1
+num,j = [], 1 # 순서변수와 소요시간을 저장할 리스트 선언
 for i in P:
-    num.append((j,i))
+    num.append((j,i)) # num에 소요시간들을 순서와 함께 복사
     j+=1
-num.sort(key=lambda x: x[1])
-Pi,res = 0,0
+num.sort(key=lambda x: x[1]) # 소요시간의 오름차순으로 정렬
+Pi,res = 0,0 # 각 소요시간을 저장할 변수와 최솟값을 저장할 변수 선언
 
 for i in num:
-    Pi += i[1]
-    res += Pi
+    Pi += i[1] # 각 Pi기준으로 소요시간을 저장하고,  
+    res += Pi # 그 Pi를 모두 더한 값(최솟값)
 sys.stdout.write("%d"%res)
