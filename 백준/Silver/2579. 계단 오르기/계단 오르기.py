7a7e37cdfef4f19d38a801af8e2ed2d2f32db7a9
YooSeungJun <12000ha@naver.com>
2024-03-23 10:19:36 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -13,6 +13,6 @@ dp[2] = step[1] + step[2]
 dp[3] = max(step[1], step[2]) + step[3]
 
 for i in range(4, N+1):
-    dp[i] = max(dp[i-3]+step[i-1]+step[i], dp[i-2]+step[i])
+    dp[i] = max(dp[i-3]+step[i-1], dp[i-2]) + step[i]
 
 print(dp[N])
