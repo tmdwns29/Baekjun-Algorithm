5fb9f536f26536e0fff025a74cd47c65828564f2
YooSeungJun <12000ha@naver.com>
2024-03-31 15:50:02 +0900

    [Gold V] Title: LCS, Time: 556 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
-
+ 
 A = input().rstrip()
 B = input().rstrip()
-LCS = [[0]*(len(A)+1) for _ in range(len(B)+1)]
-
-for i in range(1, len(B)+1):
-    for j in range(1, len(A)+1):
-        if A[j-1] == B[i-1]:
+LCS = [[0]*(len(B)+1) for _ in range(len(A)+1)]
+ 
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
             LCS[i][j] = LCS[i-1][j-1] + 1
         else:
             LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
