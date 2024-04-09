cb08f2fc0a6e4a00815d15169ed225a20884f983
YooSeungJun <12000ha@naver.com>
2024-04-09 15:16:25 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 109600 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,18 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = [0 if i**0.5%1 else 1 for i in range(N+1)]
-
-min_ = 4
-for i in range(int(N**0.5), 0, -1): # (5, 0, -1)
-    if arr[N]: # 1 4 9 16 25
-        min_ = 1
-        break
-    elif arr[N-i**2]: # 2 11 18 23 26
-        min_ = 2
-        break
-    else: # 3 5 6 7 8 10 12 13 14 15 17 19 20 21 22 24 27
-        for j in range(int((N-i**2)**0.5), 0, -1):
-            if arr[(N-i**2)-j**2]:
-                min_ = 3
-print(min_)
+dp = [0]*50001
+dp[1] = 1
+for i in range(2, N+1):
+    min_ = 4
+    j = 1
+    while j**2 <= i:
+        min_ = min(min_, dp[i-j**2])
+        j += 1
+    dp[i] = min_ + 1
+print(dp[N])
