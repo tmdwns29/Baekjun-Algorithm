a62626466a9ca548afb281f9f8674503b056c868
YooSeungJun <12000ha@naver.com>
2024-04-30 14:39:20 +0900

    [Silver I] Title: IOIOI, Time: 348 ms, Memory: 34404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,11 +6,15 @@ M = int(input())
 IOI = 'I'+('OI'*N)
 S = input().rstrip()
 
-i,cnt = 0,0
-while True:
-    if i > M-len(IOI):
-        break
-    if S[i] == 'I' and IOI == S[i:i+len(IOI)]:
+i,cnt,res = 0,0,0
+while i < M:
+    if S[i:i+3] == 'IOI':
+        i += 2
         cnt += 1
-    i += 1
-print(cnt)
+        if cnt == N:
+            cnt -= 1
+            res += 1
+    else:
+        cnt = 0
+        i += 1
+print(res)
