27cd8d1008f185c61854c6944f7660b84fc9c23d
YooSeungJun <12000ha@naver.com>
2024-05-11 23:29:12 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,13 +1,4 @@
-import sys
-input = sys.stdin.readline
-
-pieces = list(map(int, input().split()))
 chess = [1,1,2,2,2,8]
-
+pieces = list(map(int, input().split()))
 for i in range(6):
-    if chess[i] > pieces[i]:
-        sys.stdout.write("%d "%(chess[i] - pieces[i]))
-    elif chess[i] < pieces[i]:
-        sys.stdout.write("%d "%-(pieces[i]-chess[i]))
-    else:
-        sys.stdout.write("%d "%0)
+    print(chess[i]-pieces[i],end=' ')
