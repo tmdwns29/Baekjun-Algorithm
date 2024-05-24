047408199b19d631716b37b91d86dd6ea61869f0
YooSeungJun <12000ha@naver.com>
2024-05-24 09:01:32 +0900

    [Silver I] Title: 회의실 배정, Time: 232 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,17 @@
 import sys
-
 input = sys.stdin.readline
-table = []
 
 N = int(input())
+table = []
 
-for i in range(N):
-    start, end = map(int, input().split())
-    table.append((start, end))
-table.sort(key=lambda x : (x[1], x[0]))        
+for _ in range(N):
+    s, e = map(int, input().split())
+    table.append((s, e))
 
-endtime = table[0][1]
-cnt = 1
-for i in range(1, N):
-    if table[i][0] >= endtime:
-        endtime = table[i][1]
-        cnt += 1
-sys.stdout.write("%d\n"%cnt)
+table.sort(key=lambda x: (x[1],x[0]))
+
+result = [table.pop(0)]
+for i in table:
+    if result[-1][1] <= i[0]:
+        result.append(i)
+print(len(result))
