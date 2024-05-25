c3583a9615efb1be5dd1ad6cdfdc4e77810b4f93
YooSeungJun <12000ha@naver.com>
2024-05-25 22:40:12 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+num = N
+cnt = 0
+
+while True:
+    a = num//10
+    b = num%10
+    c = (a+b)%10
+    num = (b*10)+c
+
+    cnt += 1
+    if(num == N):
+        break
+print(cnt)
