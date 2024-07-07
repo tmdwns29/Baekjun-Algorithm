85fd311f898d2950140326fc74ed87f6b0aeda18
YooSeungJun <12000ha@naver.com>
2024-07-07 23:57:13 +0900

    Update Z.py

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -15,7 +15,7 @@ while N != 0:
     elif r >= 2**N and c < 2**N : # 3사분면
         res += (2**N) * (2**N) * 2
         r -= (2**N)
-    else: # 4사분면
+    else:                         # 4사분면
         res += (2**N) * (2**N) * 3
         r -= 2**N
         c -= 2**N
