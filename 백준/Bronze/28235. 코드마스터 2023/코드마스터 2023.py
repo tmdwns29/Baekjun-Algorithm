190bde5b69ad26e1e964966b908176f94635a1cd
YooSeungJun <12000ha@naver.com>
2024-10-13 23:58:05 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,6 @@
+word = input().rstrip()
+
+if word == 'SONGDO': print('HIGHSCHOOL')
+elif word == 'CODE': print('MASTER')
+elif word == '2023': print('0611')
+elif word == 'ALGORITHM': print('CONTEST')
