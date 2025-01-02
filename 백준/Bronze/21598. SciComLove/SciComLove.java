dced5f7c4df71ce491c8747c4c63f1351b3d2c9b
YooSeungJun <12000ha@naver.com>
2025-01-03 03:17:07 +0900

    [Bronze V] Title: SciComLove, Time: 172 ms, Memory: 17428 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for (int i=0; i<N; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+}
