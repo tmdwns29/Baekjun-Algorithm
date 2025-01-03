fbf74efb5d853de5076120f37a245fca216ee617
YooSeungJun <12000ha@naver.com>
2025-01-04 02:03:15 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.py
@@ -0,0 +1,12 @@
+S = input().rstrip()
+
+if S == 'fdsajkl;' or S == 'jkl;fdsa':
+    print('in-out')
+elif S == 'asdf;lkj' or S == ';lkjasdf':
+    print('out-in')
+elif S == 'asdfjkl;':
+    print('stairs')
+elif S == ';lkjfdsa':
+    print('reverse')
+else:
+    print('molu')
