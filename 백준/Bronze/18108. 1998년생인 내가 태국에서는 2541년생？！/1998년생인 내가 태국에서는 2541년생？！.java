20f98288f5c8d013d6b3a88982b33d4fd356dd10
YooSeungJun <12000ha@naver.com>
2025-01-17 00:32:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int y = input.nextInt();
+
+        System.out.println(y-543);
+    }
+}
