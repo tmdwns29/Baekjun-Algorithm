3a31059344d3eec0c25f950031a4fcddfbe38e18
YooSeungJun <12000ha@naver.com>
2025-01-17 00:38:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long A = input.nextLong();
+        long B = input.nextLong();
+        long C = input.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
