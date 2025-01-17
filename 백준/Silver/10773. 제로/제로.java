6ec908c8e7f3de273668dd4027aff751eb8f4814
YooSeungJun <12000ha@naver.com>
2025-01-17 19:49:11 +0900

    [Silver IV] Title: 제로, Time: 688 ms, Memory: 103296 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,35 +1,30 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		int K = input.nextInt();
-		int num, sum=0, cnt=-1;
-		ArrayList<Integer> arr = new ArrayList<>();
-		
-		for (int k=0; k<K; k++) {
-			num = input.nextInt();
-			
-			if (num == 0) {
-				arr.remove(cnt);
-				cnt--;
-				continue;
-			}
-			arr.add(num);
-			cnt++;
-		}
-		
-		if (arr.isEmpty()) {
-			System.out.println(0);
-		}
-		else {
-			for (int s: arr) {
-				sum += s;
-			}
-			System.out.println(sum);
-		}
-		input.close();
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int K = input.nextInt();
+        Stack<Integer> stacks = new Stack<>();
+
+        for (int k=0; k<K; k++) {
+            int num = input.nextInt();
+            if (num == 0) {
+                stacks.pop();
+                continue;
+            }
+            stacks.add(num);
+        }
+        if (stacks.isEmpty()) {
+            System.out.println(0);
+        }
+        else {
+            int answer = 0;
+            for (int s : stacks) {
+                answer += s;
+            }
+            System.out.println(answer);
+        }
+    }
 }
